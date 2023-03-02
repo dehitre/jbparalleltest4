10baf87292bc6952d188dc56f83d1908346d6b80
Ekaterina Smirnova <ame91@mail.ru>
2023-03-02 12:25:08 +0400

    devide classes

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,175 +9,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
-class CalculatorAdditionTests {
+class AddsTwoNumbers {
     @Test
     @DisplayName("1 + 1 = 2")
-    void addsTwoNumbers(){
+    void addsTwoNumbers() {
         Calculator calculator = new Calculator();
-        assertEquals(2, calculator.addition(1,1), "1 + 1 should equal 2");
+        assertEquals(2, calculator.addition(1, 1), "1 + 1 should equal 2");
     }
+
+}
+class Add {
+    @Test
     @ParameterizedTest(name = "{0} + {1} = {2}")
     @CsvSource({
             "0,1,1",
             "1,2,3",
             "21,22,50"
     })
-    void add(int a, int b, int res){
+    void add(int a, int b, int res) {
         Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " + " + b + " should equal " + res);
+        assertEquals(res, calculator.addition(a, b), () -> a + " + " + b + " should equal " + res);
     }
+
 }
-class CalculatorDivisionTests1 {
+
+class DivisionTwoNumbers {
     @Test
     @DisplayName("1 / 1 = 1")
-    void divisionTwoNumbers(){
+    void divisionTwoNumbers() {
         Calculator calculator = new Calculator();
-        assertEquals(1, calculator.division(1,1), "1 / 1 should equal 2");
+        assertEquals(1, calculator.division(1, 1), "1 / 1 should equal 2");
     }
-    @ParameterizedTest(name = "{0} / {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "1,0,0",
-            "4,2,2"
-    })
-    void division(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.division(a, b),() -> a + " / " + b + " should equal " + res);
-    }
+
 }
-class CalculatorAdditionTests1 {
+class Division {
     @Test
-    @DisplayName("1 + 1 = 2")
-    void addsTwoNumbers1(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.addition(1,1), "1 + 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} + {1} = {2}")
-    @CsvSource({
-            "0,1,1",
-            "1,2,3",
-            "21,22,50"
-    })
-    void add1(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " + " + b + " should equal " + res);
-    }
-}
-class CalculatorDivisionTests2 {
-    @Test
-    @DisplayName("1 / 1 = 1")
-    void divisionTwoNumbers2(){
-        Calculator calculator = new Calculator();
-        assertEquals(1, calculator.division(1,1), "1 / 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} / {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "1,0,0",
-            "4,2,2"
-    })
-    void division2(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.division(a, b),() -> a + " / " + b + " should equal " + res);
-    }
-}
-class CalculatorAdditionTests2 {
-    @Test
-    @DisplayName("1 + 1 = 2")
-    void addsTwoNumbers2(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.addition(1,1), "1 + 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} + {1} = {2}")
-    @CsvSource({
-            "0,1,1",
-            "1,2,3",
-            "21,22,50"
-    })
-    void add2(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " + " + b + " should equal " + res);
-    }
-}
-class CalculatorDivisionTests3 {
-    @Test
-    @DisplayName("1 / 1 = 1")
-    void divisionTwoNumbers3(){
-        Calculator calculator = new Calculator();
-        assertEquals(1, calculator.division(1,1), "1 / 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} / {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "1,0,0",
-            "4,2,2"
-    })
-    void division3(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.division(a, b),() -> a + " / " + b + " should equal " + res);
-    }
-}
-class CalculatorAdditionTests3 {
-    @Test
-    @DisplayName("1 + 1 = 2")
-    void addsTwoNumbers3(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.addition(1,1), "1 + 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} + {1} = {2}")
-    @CsvSource({
-            "0,1,1",
-            "1,2,3",
-            "21,22,50"
-    })
-    void add3(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " + " + b + " should equal " + res);
-    }
-}
-class CalculatorDivisionTests4 {
-    @Test
-    @DisplayName("1 / 1 = 1")
-    void divisionTwoNumbers4(){
-        Calculator calculator = new Calculator();
-        assertEquals(1, calculator.division(1,1), "1 / 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} / {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "1,0,0",
-            "4,2,2"
-    })
-    void division4(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.division(a, b),() -> a + " / " + b + " should equal " + res);
-    }
-}
-class CalculatorAdditionTests4 {
-    @Test
-    @DisplayName("1 + 1 = 2")
-    void addsTwoNumbers4(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.addition(1,1), "1 + 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} + {1} = {2}")
-    @CsvSource({
-            "0,1,1",
-            "1,2,3",
-            "21,22,50"
-    })
-    void add4(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " + " + b + " should equal " + res);
-    }
-}
-class CalculatorDivisionTests {
-    @Test
-    @DisplayName("1 / 1 = 1")
-    void divisionTwoNumbers(){
-        Calculator calculator = new Calculator();
-        assertEquals(1, calculator.division(1,1), "1 / 1 should equal 2");
-    }
     @ParameterizedTest(name = "{0} / {1} = {2}")
     @CsvSource({
             "0,1,0",
--- src/test/java/NewCalculatorTest.java
+++ src/test/java/NewCalculatorTest.java
@@ -9,13 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
-class CalculatorMultiplicationTests {
+class MultTwoNumbers {
     @Test
     @DisplayName("1 * 1 = 2")
-    void multTwoNumbers(){
+    void multTwoNumbers() {
         Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiplication(1,1), "1 * 1 should equal 2");
+        assertEquals(2, calculator.multiplication(1, 1), "1 * 1 should equal 2");
     }
+
+}
+class Multiplication{
+    @Test
     @ParameterizedTest(name = "{0} * {1} = {2}")
     @CsvSource({
             "0,1,0",
@@ -27,13 +31,17 @@ class CalculatorMultiplicationTests {
         assertEquals(res, calculator.addition(a, b),() -> a + " * " + b + " should equal " + res);
     }
 }
-class CalculatorSubstractionTests {
+class SubTwoNumbers {
     @Test
     @DisplayName("1 - 1 = 0")
-    void subTwoNumbers(){
+    void subTwoNumbers() {
         Calculator calculator = new Calculator();
-        assertEquals(0, calculator.subtraction(1,1), "1 - 1 should equal 2");
+        assertEquals(0, calculator.subtraction(1, 1), "1 - 1 should equal 2");
     }
+
+}
+class Substraction{
+    @Test
     @ParameterizedTest(name = "{0} - {1} = {2}")
     @CsvSource({
             "0,1,-1",
@@ -41,222 +49,6 @@ class CalculatorSubstractionTests {
             "1,1,0"
     })
     void substraction(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.subtraction(a, b),() -> a + " - " + b + " should equal " + res);
-    }
-}
-class CalculatorMultiplicationTests1 {
-    @Test
-    @DisplayName("1 * 1 = 2")
-    void multTwoNumbers1(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiplication(1,1), "1 * 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} * {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "2,2,4",
-            "4,5,25"
-    })
-    void multiplication1(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " * " + b + " should equal " + res);
-    }
-}
-class CalculatorSubstractionTests1 {
-    @Test
-    @DisplayName("1 - 1 = 0")
-    void subTwoNumbers1(){
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.subtraction(1,1), "1 - 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} - {1} = {2}")
-    @CsvSource({
-            "0,1,-1",
-            "1,0,1",
-            "1,1,0"
-    })
-    void substraction1(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.subtraction(a, b),() -> a + " - " + b + " should equal " + res);
-    }
-}
-class CalculatorMultiplicationTests2 {
-    @Test
-    @DisplayName("1 * 1 = 2")
-    void multTwoNumbers2(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiplication(1,1), "1 * 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} * {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "2,2,4",
-            "4,5,25"
-    })
-    void multiplication2(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " * " + b + " should equal " + res);
-    }
-}
-class CalculatorSubstractionTests2 {
-    @Test
-    @DisplayName("1 - 1 = 0")
-    void subTwoNumbers2(){
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.subtraction(1,1), "1 - 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} - {1} = {2}")
-    @CsvSource({
-            "0,1,-1",
-            "1,0,1",
-            "1,1,0"
-    })
-    void substraction2(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.subtraction(a, b),() -> a + " - " + b + " should equal " + res);
-    }
-}
-class CalculatorMultiplicationTests3 {
-    @Test
-    @DisplayName("1 * 1 = 2")
-    void multTwoNumbers3(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiplication(1,1), "1 * 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} * {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "2,2,4",
-            "4,5,25"
-    })
-    void multiplication3(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " * " + b + " should equal " + res);
-    }
-}
-class CalculatorSubstractionTests3 {
-    @Test
-    @DisplayName("1 - 1 = 0")
-    void subTwoNumbers3(){
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.subtraction(1,1), "1 - 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} - {1} = {2}")
-    @CsvSource({
-            "0,1,-1",
-            "1,0,1",
-            "1,1,0"
-    })
-    void substraction3(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.subtraction(a, b),() -> a + " - " + b + " should equal " + res);
-    }
-}
-class CalculatorMultiplicationTests4 {
-    @Test
-    @DisplayName("1 * 1 = 2")
-    void multTwoNumbers4(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiplication(1,1), "1 * 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} * {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "2,2,4",
-            "4,5,25"
-    })
-    void multiplication4(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " * " + b + " should equal " + res);
-    }
-}
-class CalculatorSubstractionTests4 {
-    @Test
-    @DisplayName("1 - 1 = 0")
-    void subTwoNumbers4(){
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.subtraction(1,1), "1 - 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} - {1} = {2}")
-    @CsvSource({
-            "0,1,-1",
-            "1,0,1",
-            "1,1,0"
-    })
-    void substraction4(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.subtraction(a, b),() -> a + " - " + b + " should equal " + res);
-    }
-}
-class CalculatorMultiplicationTests5 {
-    @Test
-    @DisplayName("1 * 1 = 2")
-    void multTwoNumbers5(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiplication(1,1), "1 * 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} * {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "2,2,4",
-            "4,5,25"
-    })
-    void multiplication5(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " * " + b + " should equal " + res);
-    }
-}
-class CalculatorSubstractionTests5 {
-    @Test
-    @DisplayName("1 - 1 = 0")
-    void subTwoNumbers5(){
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.subtraction(1,1), "1 - 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} - {1} = {2}")
-    @CsvSource({
-            "0,1,-1",
-            "1,0,1",
-            "1,1,0"
-    })
-    void substraction5(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.subtraction(a, b),() -> a + " - " + b + " should equal " + res);
-    }
-}
-class CalculatorMultiplicationTests6 {
-    @Test
-    @DisplayName("1 * 1 = 2")
-    void multTwoNumbers6(){
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.multiplication(1,1), "1 * 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} * {1} = {2}")
-    @CsvSource({
-            "0,1,0",
-            "2,2,4",
-            "4,5,25"
-    })
-    void multiplication6(int a, int b, int res){
-        Calculator calculator = new Calculator();
-        assertEquals(res, calculator.addition(a, b),() -> a + " * " + b + " should equal " + res);
-    }
-}
-class CalculatorSubstractionTests6 {
-    @Test
-    @DisplayName("1 - 1 = 0")
-    void subTwoNumbers6(){
-        Calculator calculator = new Calculator();
-        assertEquals(0, calculator.subtraction(1,1), "1 - 1 should equal 2");
-    }
-    @ParameterizedTest(name = "{0} - {1} = {2}")
-    @CsvSource({
-            "0,1,-1",
-            "1,0,1",
-            "1,1,0"
-    })
-    void substraction6(int a, int b, int res){
         Calculator calculator = new Calculator();
         assertEquals(res, calculator.subtraction(a, b),() -> a + " - " + b + " should equal " + res);
     }
